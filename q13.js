3cf02d049b0714c91a87fc906262011a824db88e
owias azizi <owaisazizi@jmail.com>
2023-12-16 01:35:27 +0430

    adding q13

---
+++ q13.js
@@ -0,0 +1,11 @@
+class phone {
+    constructor(color,mod){
+      this.color = color;
+      this.mod = mod;
+    }
+  }
+  b=phone.length;
+  function test(b){
+    console.log(b);
+  }
+  test(b);
