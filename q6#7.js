036403a501c7d1b48c94670b0015ea0016888ec4
owias azizi <owaisazizi@jmail.com>
2023-12-16 01:34:13 +0430

    adding q6 and q7

---
+++ q6#7.js
@@ -0,0 +1,10 @@
+const car = {
+    startengin:function(){
+        console.log('starting engin...!');
+    }
+}
+console.log(car.startengin());
+
+//q7
+Object.prototype.nationalaty = 'Afghan'
+console.log(car.nationalaty);
